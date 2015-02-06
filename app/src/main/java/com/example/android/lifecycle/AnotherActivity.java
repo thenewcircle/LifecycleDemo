2ceca4ad4655c988e93b29dff48d74ef8e1e3f2e
Dave Smith <smith@wiresareobsolete.com>
2015-02-06 13:40:39 -0700

    Convert to Studio Project. Add appcompat.

---
+++ app/src/main/java/com/example/android/lifecycle/AnotherActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.lifecycle;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class AnotherActivity extends ActionBarActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.another_activity);
+  }
+
+}
